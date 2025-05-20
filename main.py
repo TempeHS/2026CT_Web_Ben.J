0de9aded06991041cd6b5b44da28164f96672918
Ben Jones <benjamin.jones21@det.nsw.edu.au>
2025-05-20 01:36:44 +0000

    add slider

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
+    card_data = (
+        ("Card 1 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_1.png"),
+        ("Card 2 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_2.png"),
+        ("Card 3 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_3.png"),
+        ("Card 4 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_4.png"),
+        ("Card 5 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_5.png"),
+        ("Card 6 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_6.png")
+    )
+    return render_template("index.html", cards=card_data), 200
 
 
 if __name__ == '__main__':
