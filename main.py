741308aae05ac8d6f821362ef6c3922b7316d198
Ben Jones <benjamin.jones21@det.nsw.edu.au>
2025-05-21 01:55:26 +0000

    update

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Card 1 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_1.png"),
-        ("Card 2 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_2.png"),
-        ("Card 3 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_3.png"),
-        ("Card 4 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_4.png"),
-        ("Card 5 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_5.png"),
-        ("Card 6 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "images/card_image_6.png")
+        ("Card 1 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/logo.png"),
+        ("Card 2 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/card_image_2.png"),
+        ("Card 3 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/card_image_3.png"),
+        ("Card 4 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/card_image_4.png"),
+        ("Card 5 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/card_image_5.png"),
+        ("Card 6 Title", "Some quick example text to build on the card title and make up the bulk of the card's content.", "Go somewhere", "static/images/card_image_6.png")
     )
     return render_template("index.html", cards=card_data), 200
 
